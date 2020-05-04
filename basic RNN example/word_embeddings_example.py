c3e30fba294cf4a647c51f76e47fbb8269ae9a33
yash-007 <46284422+yash-007@users.noreply.github.com>
2020-05-04 22:48:52 +0530

    Update word_embeddings_example.py

--- basic RNN example/word_embeddings_example.py
+++ basic RNN example/word_embeddings_example.py
@@ -6,20 +6,16 @@ from tensorflow.keras.layers import Embedding, Dense
 from tensorflow.keras.layers import LSTM, Activation
 
 docs = ['My name is yash',
-		    'I like mango shake',
-		    'yash is computer vision guru',
-		    'artificial intelligence is awesome']
+	'I like mango shake',
+	'yash is computer vision guru',
+	'artificial intelligence is awesome']
 
 vocab_size = 10000
 max_doc_len = 7
 embedding_len = 5
 
 one_hot_representation = [one_hot(word, vocab_size) for word in docs]
-#print(one_hot_representation)
-
-one_hot_representation = pad_sequences(one_hot_representation,
-											truncating="post", padding="post", 
-											maxlen=max_doc_len)
+one_hot_representation = pad_sequences(one_hot_representation, truncating="post", padding="post", maxlen=max_doc_len)
                       
 model = Sequential()
 model.add(Embedding(vocab_size, embedding_len, input_length=max_doc_len)) 
